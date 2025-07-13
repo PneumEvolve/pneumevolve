099f21393b2a89eb48dd4e47142e85fa511c3600
Shea <sheaklipper@gmail.com>
2025-07-12 22:45:17 -0700

    Single column view?"

--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -183,7 +183,7 @@ export default function Community() {
   </CollapsibleComponent>
 )}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="flex flex-col gap-4">
         {componentOrder.map((key) => renderComponent(key))}
       </div>
     </div>
